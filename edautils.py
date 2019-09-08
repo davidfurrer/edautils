bdf95f144fbad5a19567ac572f09897979ae823c
fulowa <fulowa@gmail.com>
2019-09-08 09:11:36 +0200

    formated with black

--- edautils.py
+++ edautils.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 import math
 
-def plot_categorical(df, size_inches=(5, 7), num_cols = None):
+
+def plot_categorical(df, size_inches=(5, 7), num_cols=None):
     """Plot counts of categorical features
     Args:
         df (dataframe): dataframe containing data to be ploted
@@ -17,11 +18,11 @@ def plot_categorical(df, size_inches=(5, 7), num_cols = None):
     fig, ax = plt.subplots()
     fig.set_size_inches(size_inches)
     for i, column in enumerate(df.select_dtypes(include=['O'])):
-        plt.subplot(num_rows, num_cols, i + 1)  
+        plt.subplot(num_rows, num_cols, i + 1)
         df[column].value_counts().plot.barh()
         plt.title(column)
     return fig, ax
-    
+
 
 def plot_numerical(df, size_inches=(15, 20), num_cols=None, target_col=None):
     """Plot histograms of categorical features
@@ -36,20 +37,21 @@ def plot_numerical(df, size_inches=(15, 20), num_cols=None, target_col=None):
     num_rows = math.ceil(num_features / num_cols)
     fig, ax = plt.subplots()
     fig.set_size_inches(size_inches)
-    
+
     for i, column in enumerate(df.select_dtypes(include=['float64'])):
         plt.subplot(num_rows, num_cols, i + 1)
         if target_col == None:
-            plt.hist(df[column], alpha = 0.5)
+            plt.hist(df[column], alpha=0.5)
         else:
             target_cats = df[target_col].unique()
             for target_cat in target_cats:
-                plt.hist(df.loc[df[f'{target_col}'] == f'{target_cat}',column], alpha = 0.5, label = f'{target_cat}')
+                plt.hist(df.loc[df[f'{target_col}'] == f'{target_cat}',
+                                column], alpha=0.5, label=f'{target_cat}')
             plt.legend()
         plt.title(column)
-        
+
     return fig, ax
-    
+
 
 def summary_report(df):
     print(df.shape)
--- example.py
+++ example.py
@@ -1,32 +1,32 @@
 #%%
 import edautils
 import pandas as pd
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
-#cancer example
+# cancer example
 #%%
 df = pd.read_csv('data/data-breast-cancer.csv')
 df2 = pd.read_csv('data/mpg.csv')
 #%%
-fig, ax = edautils.plot_numerical(df, num_cols = 5)
+fig, ax = edautils.plot_numerical(df, num_cols=5)
 fig.savefig("png/numerical_example1.png", dpi=150)
-plt.close(fig) 
+plt.close(fig)
 
 #%%
-fig, ax = edautils.plot_numerical(df, num_cols = 5, target_col = 'diagnosis')
+fig, ax = edautils.plot_numerical(df, num_cols=5, target_col='diagnosis')
 fig.savefig("png/numerical_example2.png", dpi=150)
-plt.close(fig) 
+plt.close(fig)
 
 #%%
-fig, ax = edautils.plot_categorical(df, num_cols = 1)
+fig, ax = edautils.plot_categorical(df, num_cols=1)
 fig.savefig("png/categorical_example1.png", dpi=150)
-plt.close(fig) 
+plt.close(fig)
 
-#mpg example
+# mpg example
 #%%
-fig, ax = edautils.plot_categorical(df2, num_cols = 3, size_inches=(30, 10))
+fig, ax = edautils.plot_categorical(df2, num_cols=3, size_inches=(30, 10))
 fig.savefig("png/categorical_example1.2.png", dpi=150)
-plt.close(fig) 
+plt.close(fig)
 
 edautils.summary_report(df)
