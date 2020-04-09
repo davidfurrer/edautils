a1697df79687a29db5e4fa9ca433eddef6e5bd56
David Furrer <david.furrer@oto.ai>
2020-04-09 09:45:18 +0200

    adding to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 from setuptools import setup
 from setuptools import find_packages
 
-required = ['pandas', 'numpy', 'matplotlib']
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+required = ['pandas>=0.25.2', 'numpy>=1.17.0', 'matplotlib>=3.0.0']
 
 extras_required = dict()
 extras_required['tests'] = ['pytest>=4.0.0', 'pytest-pep8']
 
 setup(name='edautils',
-      version='0.1.0',
+      version='0.1.1',
       description=
       'This package contains exploratory data analysis (EDA) helper functions',
+      long_description=readme,
       author='David Furrer',
       author_email='furrer.david1@gmail.com',
+      url='https://github.com/davidfurrer/edautils',
+      license=licence
       install_requires=required,
       extras_require=extras_required,
-      packages=find_packages())
+      packages=find_packages(exclude=('tests')),
+      package_data={'edautils': ['data/*']})
